76bdf6820735e8b280c4fd652d11ab24c320c3a4
“HanFen” <tancan137@foxmail.com>
2018-12-14 21:51:03 +0800

    加入天气组件

--- Usr/Themes/index.php
+++ Usr/Themes/index.php
@@ -159,5 +159,19 @@
 		if(typeof(publiced)=="undefined") console.log("%c语音代码贡献者：学神之女。网站：https://www.dffzmxj.com","color:red;font-size:16px;");
 		var publiced = true;
 </script>
+<script>(function(T,h,i,n,k,P,a,g,e){g=function(){P=h.createElement(i);a=h.getElementsByTagName(i)[0];P.src=k;P.charset="utf-8";P.async=1;a.parentNode.insertBefore(P,a)};T["ThinkPageWeatherWidgetObject"]=n;T[n]||(T[n]=function(){(T[n].q=T[n].q||[]).push(arguments)});T[n].l=+new Date();if(T.attachEvent){T.attachEvent("onload",g)}else{T.addEventListener("load",g,false)}}(window,document,"script","tpwidget","//widget.seniverse.com/widget/chameleon.js"))</script>
+<script>tpwidget("init", {
+    "flavor": "bubble",
+    "location": "WX4FBXXFKE4F",
+    "geolocation": "enabled",
+    "position": "bottom-right",
+    "margin": "10px 10px",
+    "language": "auto",
+    "unit": "c",
+    "theme": "chameleon",
+    "uid": "U7CB28E99F",
+    "hash": "8adcf9ff20a03efd6b683583d61ac7e4"
+});
+tpwidget("show");</script>
     </body>
 </html>
